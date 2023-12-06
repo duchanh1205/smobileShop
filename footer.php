ec3323227aaf670efc8a7dd4fb1f6d3c66f6742e
nguyenMinhDuc <duchanh12052001@gmail.com>
2023-12-06 14:00:37 +0700

    first commit

---
+++ footer.php
@@ -0,0 +1,83 @@
+<?php 
+    
+    ?>
+    <!--::footer_part start::-->
+    <footer class="footer_part" style=" margin-top: 10px; padding: 20px; background-color: #F8F8F8; background-clip: padding;">
+        <div class="container">
+            <div class="row justify-content-around">
+                <div class="col-sm-6 col-lg-3">
+                    <div class="single_footer_part">
+                        <h4>Thông Tin Cửa Hàng</h4>
+                        <ul class="list-unstyled">
+                            <li><a>Cửa Hàng Smobile</a></li>
+                            <li><a>1A6 P. Cù Chính Lan, Khương Thượng, Thanh Xuân, Hà Nội</a></li>
+                            <li><a>Hotline: 0904.078.692</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <div class="col-sm-6 col-lg-3">
+                    <div class="single_footer_part">
+                        <h4>Chính Sách</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="">Quy chế hoạt động</a></li>
+                            <li><a href="">Chính sách Bảo hành</a></li>
+                            <li><a href="">Liên hệ hợp tác kinh doanh</a></li>
+                            <li><a href="">Đơn Doanh nghiệp</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="single_footer_part">
+                        <h4>Bảo Hành</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="">Tra thông tin bảo hành</a></li>
+                            <li><a href="">Trung tâm bảo hành chính hãng</a></li>
+                            <li><a href="">Quy định về việc sao lưu dữ liệu</a></li>
+                            <li><a href="">Dịch vụ bảo hành điện thoại</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-3">
+                    <div class="single_footer_part">
+                        <h4>Hotline</h4>
+                        <ul class="list-unstyled">
+                            <li><a >Gọi mua hàng: 0904.078.692</a></li>
+                            <li><a >Gọi kiếu nại: 0986.613.305</a></li>
+                            <li><a >Gọi bảo hành: 0352.353.191</a></li>
+                            <li><a >(8h00 - 22h00)</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="copyright_part">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8">
+                        <div class="copyright_text">
+                            <P>
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                Copyright &copy;
+                                <script>
+                                    document.write(new Date().getFullYear());
+                                </script> All rights reserved <i class="ti-heart" aria-hidden="true"></i> by <a href="https://www.facebook.com/nmd12o5" target="_blank">Smobile</a>
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                            </P>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="footer_icon social_icon">
+                            <ul class="list-unstyled">
+                                <li><a href="https://www.facebook.com/nmd12o5" class="single_social_icon"><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a href="https://www.instagram.com/andiamdee_1205/" class="single_social_icon"><i class="fab fa-instagram"></i></a></li>
+                        
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--::footer_part end::-->
